677d27f882689eec84abb9fc4437ad1bc4557a89
kikoorikoo <alexandrakardaras@gmail.com>
2015-12-29 18:12:10 -0500

    updated front-page, created archive-case_studies

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for single case studies pages
+ * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -8,49 +8,39 @@
  * different template.
  *
  * @package WordPress
- * @subpackage Accelerate
- * @since Accelerate 1.1
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
  */
 
 get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-
 			<?php while ( have_posts() ) : the_post(); 
-				$size = "full";
-				$services = get_field('services');
-				$client = get_field('client');
-				$site_link = get_field('site_link');
-				$image_1 = get_field('image_1');
-				$image_2 = get_field('image_2');
-				$image_3 = get_field('image_3');
-			?>
 
-				<article class="case-study">
+			$size = "full";
+			$services = get_field('services');
+			$image_1 = get_field('image_1');
+
+
+			?>
+			<article class="case-study">
 					<aside class="case-study-sidebar">
-						<h1><?php echo the_title(); ?></h1>
+						<h1><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h1>
 
 						<h2><?php echo $services; ?></h2>
-						<h5>Client: <?php echo $client; ?></h5>
-						<?php the_content(); ?>
-						<p><strong><a href="<?php echo $site_link; ?>">Visit Live Site</a></strong></p>
+						<?php the_excerpt(); ?>
+						<p><strong><a href="<?php the_permalink(); ?>">View Project</a>&nbsp;<span>&rsaquo;</span></strong></p>
 					</aside>
 
 
 					<div class="case-study-content">
 						<div class="case-study-images">
+						<a href="<?php the_permalink(); ?>">
 							<?php if($image_1) { ?>
 							<?php echo wp_get_attachment_image( $image_1, $size ); ?>
 							<?php } ?>
-
-							<?php if($image_2) { ?>
-							<?php echo wp_get_attachment_image( $image_2, $size ); ?>
-							<?php } ?>
-
-							<?php if($image_3) { ?>
-							<?php echo wp_get_attachment_image( $image_3, $size ); ?>
-							<?php } ?>
+							</a>
 						</div>
 					</div>
 				</article>
